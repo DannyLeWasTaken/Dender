5521e6f9d41bfaef6529e08ec57b1998ecda3fca
Danny <dannylewastaken@protonmail.com>
2023-01-13 17:00:17 -0500

    Added tinygltf

---
+++ src/AppWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Danny on 2023-01-13.
+//
+
+#include "AppWindow.hpp"
---
+++ src/AppWindow.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Danny on 2023-01-13.
+//
+
+#ifndef DENDER_APPWINDOW_HPP
+#define DENDER_APPWINDOW_HPP
+
+class AppWindow {
+
+};
+
+
+#endif //DENDER_APPWINDOW_HPP
