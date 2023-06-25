de7bd5a6a030f294fbbfb247fa7a83be630afc25
Danny <dannylewastaken@protonmail.com>
2023-06-24 20:37:31 -0400

    Improve results

--- src/util/result.hpp
+++ src/util/result.hpp
@@ -5,26 +5,34 @@
 #ifndef DENDER_RESULT_HPP
 #define DENDER_RESULT_HPP
 
-#include <functional>
+#include <variant>
+#include <optional>
+#include <stdexcept>
 
-template <typename T, typename E>
+template <typename Ok, typename Err>
 struct Result {
-	T Ok;
-	E Error;
+public:
+	Result(Ok ok_value) : result(ok_value) {}
+	Result(Err err_value) : result(err_value) {}
 
-	T expect(
-			const std::function<void(bool)>& error_handler
-			) {
-		if (Error) {
-			error_handler(this->Error);
+	std::optional<Ok> unwrap() {
+		if (std::holds_alternative<Ok>(result)) {
+			return std::get<Ok>(result);
 		} else {
-			return this->Ok;
+			throw std::runtime_error("Unwrapped called for an error result");
 		}
 	}
 
-	T unwrap() {
-		return this->Ok;
+	std::optional<Ok> expect(const std::string& error_message) {
+		if (std::holds_alternative<Ok>(result)) {
+			return std::get<Ok>(result);
+		} else {
+			throw std::runtime_error(error_message);
+		}
 	}
+
+private:
+	std::variant<Ok, Err> result;
 };
 
 #endif//DENDER_RESULT_HPP
