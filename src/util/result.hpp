5e267c2de09c54696982cfb3386da62140d3c3ff
Danny <dannylewastaken@protonmail.com>
2023-06-24 20:51:31 -0400

    Add default type & default value if type is selected

--- src/util/result.hpp
+++ src/util/result.hpp
@@ -9,30 +9,32 @@
 #include <optional>
 #include <stdexcept>
 
-template <typename Ok, typename Err>
+template <typename T, typename E = bool>
 struct Result {
 public:
-	Result(Ok ok_value) : result(ok_value) {}
-	Result(Err err_value) : result(err_value) {}
+	Result(T ok_value) : result(ok_value) {}
+	Result(E err_value) : result(err_value) {}
 
-	std::optional<Ok> unwrap() {
-		if (std::holds_alternative<Ok>(result)) {
-			return std::get<Ok>(result);
+	Result(std::enable_if_t<std::is_same<E, bool>::value, E> err_value = true): result(err_value) {}
+
+	std::optional<T> unwrap() {
+		if (std::holds_alternative<T>(result)) {
+			return std::get<T>(result);
 		} else {
 			throw std::runtime_error("Unwrapped called for an error result");
 		}
 	}
 
-	std::optional<Ok> expect(const std::string& error_message) {
-		if (std::holds_alternative<Ok>(result)) {
-			return std::get<Ok>(result);
+	std::optional<T> expect(const std::string& error_message) {
+		if (std::holds_alternative<T>(result)) {
+			return std::get<T>(result);
 		} else {
 			throw std::runtime_error(error_message);
 		}
 	}
 
 private:
-	std::variant<Ok, Err> result;
+	std::variant<T, E> result;
 };
 
 #endif//DENDER_RESULT_HPP
